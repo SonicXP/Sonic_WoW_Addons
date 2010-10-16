9d41b2721521f3847d84d9f000573e6a7553f529
SonicXP <sonic.xp@163.com>
2010-10-17 00:01:08 +0800

    [For WoW 4.0.1] Sonic_Buffs 4.0 beta version

---
+++ Sonic_Buffs/SBuff_BuffTimer.lua
@@ -0,0 +1,86 @@
+﻿-- show timer as xx:yy format
+SecondsToTimeAbbrev = function(seconds)
+    local m,s;
+    if ( seconds > 6000 ) then
+        m = floor(seconds / 60);
+        s = floor(seconds-(m)*60);
+        return format("|cff00ff00%03d:%02d|r", m, s);
+    elseif ( seconds > 600 ) then
+        m = floor(seconds / 60);
+        s = floor(seconds-(m)*60);
+        return format("|cff00ff00%02d:%02d|r", m, s);
+    elseif ( seconds > 60 ) then
+        m = floor(seconds / 60);
+        s = floor(seconds-(m)*60);
+        return format("|cff00ff00%01d:%02d|r", m, s);
+    elseif ( seconds >1 ) then
+        m = floor(seconds / 60);
+        s = floor(seconds-(m)*60);
+        return format("|cffff0000%01d:%02d|r", m, s);
+    end
+    return "N/A";
+end;
+
+-- change buff timer font
+local fontPath = "Fonts\\ARIALN.TTF";
+local fontSize = 12;
+local fontOutline = "OUTLINE";
+
+local timer_font_frame = CreateFrame("Frame", "SBuff_BuffTimerFrame", UIParent);
+timer_font_frame:RegisterEvent("UNIT_AURA");
+timer_font_frame:RegisterEvent("PLAYER_ENTERING_WORLD");
+
+local SBuff_SetBuffFont = function(self, event, ...)
+
+    local unit = ...;
+    if (event == "UNIT_AURA" and unit ~= "player") then
+        return;
+    end;
+
+    if (event == "PLAYER_ENTERING_WORLD") then
+        self:UnregisterEvent("PLAYER_ENTERING_WORLD");
+    end;
+
+    local i;
+    local tmpDur;
+    --BUFF
+    for i=1,32,1 do
+        tmpDur = getglobal("BuffButton"..i.."Duration");
+        if tmpDur then
+            tmpDur:SetFont(fontPath, fontSize, fontOutline);
+        end;
+    end;
+    --DEBUFF
+    for i=1,16,1 do
+        tmpDur = getglobal("DebuffButton"..i.."Duration");
+        if tmpDur then
+            tmpDur:SetFont(fontPath, fontSize, fontOutline);
+        end;
+    end;
+    --ENCHANT
+    for i=1,2,1 do
+        tmpDur = getglobal("TempEnchant"..i.."Duration");
+        if tmpDur then
+            tmpDur:SetFont(fontPath, fontSize, fontOutline);
+        end;
+    end;
+end;
+
+timer_font_frame:SetScript("OnEvent", SBuff_SetBuffFont);
+
+--[[
+--show N/A
+hooksecurefunc("AuraButton_Update", function(buttonName, index, filter)
+    local unit = PlayerFrame.unit;
+    local name, _, _, _, _, _, expirationTime = UnitAura(unit, index, filter);
+
+    if (name) and (expirationTime == 0) and (SHOW_BUFF_DURATIONS == "1") then
+        local buffName = buttonName..index;
+        local buff = _G[buffName];
+        local buffDuration = _G[buffName.."Duration"];
+
+        buffDuration:SetText("|cff00ff00N/A|r");
+        buffDuration:Show();
+    end;
+end);
+]]
---
+++ Sonic_Buffs/SBuff_Buffs.lua
@@ -0,0 +1,102 @@
+﻿SBuff_Version = "4.0 Beta";
+
+local SBuff_MAX_PARTY_BUFFS = 12;
+local SBuff_MAX_PARTY_DEBUFFS = 8;
+local SBuff_MAX_PET_BUFFS = 8;
+local SBuff_MAX_PET_DEBUFFS = 8;
+
+-- change system original variables
+MAX_PARTY_BUFFS = SBuff_MAX_PARTY_BUFFS;
+MAX_PARTY_DEBUFFS = SBuff_MAX_PARTY_DEBUFFS;
+
+local function CreatePartyBuffs()
+    local i;
+    for i = 1,4,1 do
+        local buff;
+        local party = "PartyMemberFrame"..i;
+        local db;
+        --Buff
+        for j = 1, SBuff_MAX_PARTY_BUFFS, 1 do
+            buff = CreateFrame("Button", party.."Buff"..j, getglobal(party), "PartyBuffFrameTemplate");
+            buff:SetID(j);
+            buff:ClearAllPoints();
+            if j == 1 then
+                buff:SetPoint("TOPLEFT", party, "TOPLEFT", 48, -32);
+            else
+                buff:SetPoint("LEFT", party.."Buff"..j-1, "RIGHT", 2, 0);
+            end;
+        end
+        --Debuff
+        db = getglobal(party.."Debuff1");
+        db:ClearAllPoints();
+        db:SetPoint("LEFT", party, "RIGHT", -5, 5);
+        for j = 5, SBuff_MAX_PARTY_DEBUFFS, 1 do
+            buff = CreateFrame("Button", party.."Debuff"..j, getglobal(party), "PartyDebuffFrameTemplate");
+            buff:SetID(j);
+            buff:ClearAllPoints();
+            buff:SetPoint("LEFT", party.."Debuff"..j-1, "RIGHT", 2, 0);
+        end
+    end
+end
+
+local function PetDebuffOnEnter(self)
+    GameTooltip:SetOwner(self, "ANCHOR_RIGHT");
+    GameTooltip:SetUnitDebuff(PetFrame.unit, self:GetID());
+end
+
+local function PetBuffOnEnter(self)
+    GameTooltip:SetOwner(self, "ANCHOR_RIGHT");
+    GameTooltip:SetUnitBuff(PetFrame.unit, self:GetID());
+end
+
+local function CreatePetBuffs()
+    --Buff
+    for j = 1, SBuff_MAX_PET_BUFFS, 1 do
+        buff = CreateFrame("Button", "PetFrameBuff"..j, PetFrame, "PartyBuffFrameTemplate");
+        buff:SetID(j);
+        buff:ClearAllPoints();
+        buff:SetScript("OnEnter",PetBuffOnEnter);
+        if j == 1 then
+            buff:SetPoint("TOPLEFT", "PetFrame", "TOPLEFT", 48, -42);
+        else
+            buff:SetPoint("LEFT", "PetFrameBuff"..j-1, "RIGHT", 2, 0);
+        end;
+    end
+    --Debuff
+    PetFrameDebuff1:ClearAllPoints();
+    --PetFrameDebuff1:SetPoint("LEFT", "PetFrame", "RIGHT", -7, -5);
+    PetFrameDebuff1:SetPoint("TOP", "PetFrameBuff1", "BOTTOM", 0, -2);
+    for j = 5, SBuff_MAX_PET_DEBUFFS, 1 do
+        buff = CreateFrame("Button", "PetFrameDebuff"..j, PetFrame, "PartyDebuffFrameTemplate");
+        buff:SetID(j);
+        buff:ClearAllPoints();
+        buff:SetScript("OnEnter",PetDebuffOnEnter);
+        buff:SetPoint("LEFT", "PetFrameDebuff"..j-1, "RIGHT", 2, 0);
+    end
+end
+
+--Create BuffFrame and DebuffFrame
+CreatePartyBuffs();
+CreatePetBuffs();
+
+--Hide BuffTooltip
+PartyMemberBuffTooltip_Update = function(self)
+    return;
+end
+
+--Hook RefreshDebuffs func
+local old_RefreshDebuffs = RefreshDebuffs;
+RefreshDebuffs = function(frame, unit, numDebuffs, suffix, checkCVar)
+    local name = frame:GetName();
+    numDebuffs = numDebuffs or 4;
+
+    if string.find(name, "^PartyMemberFrame%d$") then
+        old_RefreshDebuffs(frame, unit, SBuff_MAX_PARTY_DEBUFFS);
+        RefreshBuffs(frame, unit, SBuff_MAX_PARTY_BUFFS);
+    elseif (name == "PetFrame") then
+        old_RefreshDebuffs(frame, unit, SBuff_MAX_PET_DEBUFFS);
+        RefreshBuffs(frame, unit, SBuff_MAX_PET_BUFFS);
+    else
+        old_RefreshDebuffs(frame, unit, numDebuffs, suffix, checkCVar);
+    end;
+end
