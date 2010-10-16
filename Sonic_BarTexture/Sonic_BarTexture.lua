cdcee9c76eea3d556014cc5e7ebe7e60b2af3702
SonicXP <sonic.xp@163.com>
2010-10-17 00:14:34 +0800

    [For WoW 4.0.1] Sonic_BarTexture 4.0 beta version

---
+++ Sonic_BarTexture/Sonic_BarTexture.lua
@@ -0,0 +1,41 @@
+﻿SBT_Version = "4.0 Beta";
+SBT_Frame = {
+    "PlayerFrameHealthBar",
+    "PlayerFrameManaBar",
+    "TargetFrameHealthBar",
+    "TargetFrameManaBar",
+    "PetFrameHealthBar",
+    "PetFrameManaBar",
+    "PartyMemberFrame1HealthBar",
+    "PartyMemberFrame1ManaBar",
+    "PartyMemberFrame2HealthBar",
+    "PartyMemberFrame2ManaBar",
+    "PartyMemberFrame3HealthBar",
+    "PartyMemberFrame3ManaBar",
+    "PartyMemberFrame4HealthBar",
+    "PartyMemberFrame4ManaBar",
+    "PartyMemberFrame1PetFrameHealthBar",
+    "PartyMemberFrame2PetFrameHealthBar",
+    "PartyMemberFrame3PetFrameHealthBar",
+    "PartyMemberFrame4PetFrameHealthBar",
+    "TargetofTargetHealthBar",
+    "TargetofTargetManaBar",
+};
+
+function SBT_SetTexture()
+    local tempBar;
+    local v;
+    for _,v in pairs(SBT_Frame) do
+        tempBar = getglobal(v);
+        if (tempBar) then
+            tempBar:SetStatusBarTexture("Interface\\AddOns\\Sonic_BarTexture\\StatusBar");
+        end;
+    end;
+end
+
+local frame = CreateFrame("Frame", "Sonic_BarTextureFrame", UIParent);
+frame:RegisterEvent("PLAYER_ENTERING_WORLD");
+frame:SetScript("OnEvent", function(self)
+    SBT_SetTexture();
+    self:UnregisterEvent("PLAYER_ENTERING_WORLD");
+end);
